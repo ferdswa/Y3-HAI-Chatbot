877e377ecfbcfe9d9508a17920201ff345bebecb
ferdswa <maximcarr03@hotmail.com>
2025-12-05 18:57:43 +0000

    -a: Add/Append/RemoveFrom/Delete/Shuffle/Clear playlist functionality

--- taskProcessing.py
+++ taskProcessing.py
@@ -1,11 +1,13 @@
 import csv
 import os
 from pathlib import Path
+import random
 
 listOfPlayLists: dict[str,list[str]] = {}
 
 class PlaylistManager:
     playlistDir = os.path.dirname(os.path.abspath(__file__))+os.sep+"playlists"+os.sep
+    #Read in playlists
     def __init__(self):
         try:
             for file in os.listdir(self.playlistDir):
@@ -17,9 +19,75 @@ class PlaylistManager:
                     for songList in readPlaylist:
                         songs=songList
                     listOfPlayLists[Path(file).stem] = songs
-            print (listOfPlayLists)
         except FileNotFoundError:#No file playlist -> no playlists
             os.mkdir(self.playlistDir)
 
+    def createPlaylist(self,playlistName):
+        playlistPath = self.playlistDir+playlistName
+        f = open(playlistPath+".csv",'a')
+        listOfPlayLists[playlistName] = []
+    
+    def addToPlaylist(self, playlistName, songs:list[str]):
+        addedSongs = []
+        playlistPath = self.playlistDir+playlistName
+        with open(playlistPath+".csv", "w") as playlistCSV:
+            for song in songs:
+                if song in listOfPlayLists[playlistName]:
+                    print(f"{song} is already in playlist {playlistName}!")
+                else:
+                    listOfPlayLists[playlistName].append(song)
+                    addedSongs.append(song)
+            for song in listOfPlayLists[playlistName]:
+                if song != listOfPlayLists[playlistName][len(listOfPlayLists[playlistName])-1]:
+                    playlistCSV.write(song+",")
+                else:
+                    playlistCSV.write(song)
+        print(f"{addedSongs} have been added to {playlistName}")
+    
+    def removeFromPlaylist(self, playlistName, songs:list[str]):
+        delSongs = []
+        playlistPath = self.playlistDir+playlistName
+        with open(playlistPath+".csv", "w") as playlistCSV:
+            for song in songs:
+                if song in listOfPlayLists[playlistName]:
+                    listOfPlayLists[playlistName].remove(song)
+                    delSongs.append(song)
+                else:
+                    print(f"{song} is not present in {playlistName}!")
+            for song in listOfPlayLists[playlistName]:
+                if song != listOfPlayLists[playlistName][len(listOfPlayLists[playlistName])-1]:
+                    playlistCSV.write(song+",")
+                else:
+                    playlistCSV.write(song)
+        print(f"{delSongs} have been removed from {playlistName}")
 
-pm = PlaylistManager()
+    def shuffle(self, playlistName):
+        random.shuffle(listOfPlayLists[playlistName])
+        print(f"{playlistName} shuffled")
+    
+    def deletePlaylist(self, playlistName):
+        playlistPath = self.playlistDir+playlistName+".csv"
+        os.remove(playlistPath)
+        print(f"{playlistName} deleted")
+    
+    def clearPlaylist(self, playlistName):
+        listOfPlayLists[playlistName] = []
+        print(f"{playlistName} cleared")
+
+    def showPlaylist(self, playlistName):
+        songs = []
+        for song in listOfPlayLists[playlistName]:
+            songs.append(song)
+        print(songs)
+
+#testing - remember to delete
+# pm = PlaylistManager()
+# pm.createPlaylist("hello")
+# pm.addToPlaylist("hello",["these","are","some","test","songs"])
+# pm.showPlaylist("hello")
+# pm.addToPlaylist("hello",["these"])
+# pm.showPlaylist("hello")
+# pm.removeFromPlaylist("hello",["are","some"])
+# pm.showPlaylist("hello")
+# pm.removeFromPlaylist("hello",["some"])
+# pm.showPlaylist("hello")
